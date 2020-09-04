a1e67550c32af0bd70d04c902dfa9f03a22e6dfa
aciubotaru <andrei.ciubotaru@ness.com>
2020-09-04 10:00:44 +0300

    fixed python errors

--- tests/test_java.py
+++ tests/test_java.py
@@ -2,6 +2,7 @@
 import pytest
 from hamcrest import contains_string, assert_that
 
+# pylint: disable=redefined-outer-name
 @pytest.fixture()
 def get_ansible_vars(host):
     """Define AnsibleVars"""
@@ -10,17 +11,17 @@ def get_ansible_vars(host):
     ansible_vars.update(host.ansible("include_vars", java_role)["ansible_facts"]["java_role"])
     return ansible_vars
 
-def test_java_exists(host, ansible_vars):
+def test_java_exists(host, get_ansible_vars):
     "Check that java executable exists"
-    cmd = host.run("[ -f /opt/openjdk-{}/bin/java ] && echo \"File exists\" || echo \"File does not exists\"".format(ansible_vars["jdk_version"]))
+    cmd = host.run("[ -f /opt/openjdk-{}/bin/java ] && echo \"File exists\" || echo \"File does not exists\"".format(get_ansible_vars["jdk_version"]))
     assert_that(cmd.stdout, contains_string("File exists"))
 
-def test_java_version(host, ansible_vars):
+def test_java_version(host, get_ansible_vars):
     "Check that java version is correct"
-    cmd = host.run("/opt/openjdk-{}/bin/java -version".format(ansible_vars["jdk_version"]))
-    assert_that(cmd.stderr, contains_string(ansible_vars["jdk_version"]))
+    cmd = host.run("/opt/openjdk-{}/bin/java -version".format(get_ansible_vars["jdk_version"]))
+    assert_that(cmd.stderr, contains_string(get_ansible_vars["jdk_version"]))
 
-def test_java_home(host, ansible_vars):
+def test_java_home(host, get_ansible_vars):
     "Check that JAVA_HOME is environment variable is set"
-    cmd = host.run("source {}/setenv.sh && echo $JAVA_HOME".format(ansible_vars["config_dir"]))
-    assert_that(cmd.stdout, contains_string("/opt/openjdk-{}".format(ansible_vars["jdk_version"])))
+    cmd = host.run("source {}/setenv.sh && echo $JAVA_HOME".format(get_ansible_vars["config_dir"]))
+    assert_that(cmd.stdout, contains_string("/opt/openjdk-{}".format(get_ansible_vars["jdk_version"])))
