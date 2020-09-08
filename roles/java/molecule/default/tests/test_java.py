46800cb6c2d5f27a736bcfded9ccfb2409bf0da7
lsuciu <suciulucian@gmail.com>
2020-09-08 10:06:08 +0300

    Opsexp 446   create java role (#2)
    * Updated single/multi machine diagrams and added HA diagram.
    
    * Fixed lint issues and added java role tests.
    
    * Created travis yaml and dependencies. fixed various lint problems
    
    * edited travis.yaml
    
    * fixed pylint error
    
    * added molecule/ec2 configs
    
    * changed paths
    
    * escaped special chars in nexus credentials
    
    * escaped more characters
    
    * fixed python errors
    
    * Moved tests to the java role folder and created prerequisites for them to run
    
    * fixed lint errors
    
    * installed molecule for the unit test stage
    
    * updated trvais.yml
    
    * testing travis build
    
    * testing unit tests in travis
    
    * testing molecule[docker]
    
    * fixed typo
    
    * Testing again molecule[docker]
    
    * trying something else
    
    * simplified travis build
    
    * clean up
    
    * added become_user: root
    
    * removed become:true
    
    Co-authored-by: Gavin Cornwell <gavin.cornwell@alfresco.com>
    Co-authored-by: aciubotaru <andrei.ciubotaru@ness.com>

---
+++ roles/java/molecule/default/tests/test_java.py
@@ -0,0 +1,26 @@
+"""Java Tests"""
+import pytest
+from hamcrest import contains_string, assert_that
+
+# pylint: disable=redefined-outer-name
+@pytest.fixture()
+def get_ansible_vars(host):
+    """Define AnsibleVars"""
+    java_role = "file=../../vars/main.yml name=java_role"
+    ansible_vars = host.ansible("include_vars", java_role)["ansible_facts"]["java_role"]
+    ansible_vars.update(host.ansible("include_vars", java_role)["ansible_facts"]["java_role"])
+    return ansible_vars
+
+def test_java_exists(host, get_ansible_vars):
+    "Check that java executable exists"
+    assert_that(host.file("/opt/openjdk-{}/bin/java".format(get_ansible_vars["jdk_version"])).exists, get_ansible_vars["jdk_version"])
+
+def test_java_version(host, get_ansible_vars):
+    "Check that java version is correct"
+    cmd = host.run("/opt/openjdk-{}/bin/java -version".format(get_ansible_vars["jdk_version"]))
+    assert_that(cmd.stderr, contains_string(get_ansible_vars["jdk_version"]))
+
+def test_java_home(host, get_ansible_vars):
+    "Check that JAVA_HOME is environment variable is set"
+    cmd = host.run("source {}/setenv.sh && echo $JAVA_HOME".format(get_ansible_vars["config_dir"]))
+    assert_that(cmd.stdout, contains_string("/opt/openjdk-{}".format(get_ansible_vars["jdk_version"])))
