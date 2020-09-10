cab65c2850b1f02152e4fb2d7ef35a74f461adbb
Andrei Ciubotaru <andrei.ciubotaru@ness.com>
2020-09-10 16:50:17 +0300

    fixed current tests

--- roles/java/molecule/default/tests/test_java.py
+++ roles/java/molecule/default/tests/test_java.py
@@ -7,8 +7,12 @@ from hamcrest import contains_string, assert_that
 def get_ansible_vars(host):
     """Define AnsibleVars"""
     java_role = "file=../../vars/main.yml name=java_role"
+    common_vars = "../../../common/vars/main.yml name=common_vars"
+    common_defaults = "../../../common/defaults/main.yml name=common_defaults"
     ansible_vars = host.ansible("include_vars", java_role)["ansible_facts"]["java_role"]
     ansible_vars.update(host.ansible("include_vars", java_role)["ansible_facts"]["java_role"])
+    ansible_vars.update(host.ansible("include_vars", common_vars)["ansible_facts"]["common_vars"])
+    ansible_vars.update(host.ansible("include_vars", common_defaults)["ansible_facts"]["common_defaults"])
     return ansible_vars
 
 def test_java_exists(host, get_ansible_vars):
