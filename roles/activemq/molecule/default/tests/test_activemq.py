9aa8435233e0b9a707f19ed3d937bf9c31502488
Andrei Ciubotaru <andrei.ciubotaru@ness.com>
2020-09-16 12:25:23 +0300

    Added unit tests for ActiveMQ role

--- roles/activemq/molecule/default/tests/test_activemq.py
+++ roles/activemq/molecule/default/tests/test_activemq.py
@@ -1,6 +1,6 @@
 """Java Tests"""
 import pytest
-from hamcrest import assert_that
+from hamcrest import assert_that, contains_string
 
 # pylint: disable=redefined-outer-name
 @pytest.fixture()
@@ -20,3 +20,24 @@ def get_ansible_vars(host):
 def test_activemq_exe_exists(host, get_ansible_vars):
     "Check that ActiveMQ executable exists"
     assert_that(host.file("/opt/apache-activemq-{}/bin/activemq".format(get_ansible_vars["activemq_version"])).exists, get_ansible_vars["activemq_home"])
+
+def test_activemq_version(host, get_ansible_vars):
+    "Check that ActiveMQ version is correct"
+    cmd = host.run("source {}/setenv.sh && $ACTIVEMQ_HOME/bin/activemq --version".format(get_ansible_vars["config_folder"]))
+    assert_that(cmd.stdout, contains_string("ActiveMQ {}".format(get_ansible_vars["activemq_version"])))
+
+def test_activemq_home(host, get_ansible_vars):
+    "Check that ActiveMQ home is set correctly"
+    cmd = host.run("source {}/setenv.sh && echo $ACTIVEMQ_HOME".format(get_ansible_vars["config_folder"]))
+    assert_that(cmd.stdout, contains_string("/opt/apache-activemq-{}".format(get_ansible_vars["activemq_version"])))
+
+def test_activemq_service(host, get_ansible_vars):
+    "Check that ActiveMQ is enabled and running"
+    assert_that(host.service("activemq").is_running)
+    assert_that(host.service("activemq").is_enabled)
+
+def test_activemq_web_console(host, get_ansible_vars):
+    "Check that ActiveMQ web console is available and returns a HTTP 200 for the home page"
+    cmd = host.run("curl -iL --user admin:admin http://{}:8161".format(get_ansible_vars["activemq_host"]))
+    assert_that(cmd.stdout, contains_string("Welcome to the Apache ActiveMQ!"))
+    assert_that(cmd.stdout, contains_string("200 OK"))
