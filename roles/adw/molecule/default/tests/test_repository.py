9193e2821d994a1662d63b0a240651a283c60b0c
Lucian Suciu <lucian.suciu@ness.com>
2020-09-17 11:56:26 +0300

    digital-workspace path

--- roles/adw/molecule/default/tests/test_repository.py
+++ roles/adw/molecule/default/tests/test_repository.py
@@ -7,16 +7,10 @@ from hamcrest import assert_that, contains_string
 def AnsibleVars(host):
     """Define AnsibleVars"""
     adw_role = "file=../../vars/main.yml name=adw_role"
-    repository_role = "file=../../../roles/repository/vars/main.yml name=repository_role"
-    tomcat_role = "file=../../../roles/tomcat/vars/main.yml name=tomcat_role"
-    java_role = "file=../../../roles/java/vars/main.yml name=java_role"
-    ansible_vars = host.ansible("include_vars", tomcat_role)["ansible_facts"]["tomcat_role"]
-    ansible_vars.update(host.ansible("include_vars", adw_role)["ansible_facts"]["adw_role"])
-    ansible_vars.update(host.ansible("include_vars", repository_role)["ansible_facts"]["repository_role"])
-    ansible_vars.update(host.ansible("include_vars", java_role)["ansible_facts"]["java_role"])
+    ansible_vars = host.ansible("include_vars", adw_role)["ansible_facts"]["adw_role"]
     return ansible_vars
 
 def test_share_context_200(host, AnsibleVars):
     "Check that /share context is available and returns a HTTP 200 status code"
-    cmd = host.run("curl -iL --user admin:admin http://{}:8080/workspace".format(AnsibleVars["repository"]["host"]))
+    cmd = host.run("curl -iL --user admin:admin http://{}:8080/digital-workspace".format(AnsibleVars["adw_host"]))
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
