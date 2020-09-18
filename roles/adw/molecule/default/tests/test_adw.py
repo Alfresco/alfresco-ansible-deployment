d8f8d2cff088bbda2c99eca1e5e4b49c733048c2
Lucian Suciu <lucian.suciu@ness.com>
2020-09-18 14:53:39 +0300

    fix

--- roles/adw/molecule/default/tests/test_adw.py
+++ roles/adw/molecule/default/tests/test_adw.py
@@ -7,7 +7,7 @@ from hamcrest import assert_that, contains_string
 def AnsibleVars(host):
     """Define AnsibleVars"""
     common_vars = "../../../common/vars/main.yml name=common_vars"
-    ansible_vars = host.ansible("include_vars", adw_role)["ansible_facts"]["common_vars"]
+    ansible_vars = host.ansible("include_vars", common_vars)["ansible_facts"]["common_vars"]
     return ansible_vars
 
 def test_digital_workspace_200(host, AnsibleVars):
