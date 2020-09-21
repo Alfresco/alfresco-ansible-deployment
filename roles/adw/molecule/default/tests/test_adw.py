48058ed885df0e6a3699d981db34a8a0afaee8ea
lsuciu <suciulucian@gmail.com>
2020-09-21 11:50:06 +0300

    OPSEXP-453 (#13)
    * OPSEXP-451-share-tests
    
    * lint
    
    * OPSEXP-453-adw
    
    * lint
    
    * lint
    
    * clean irelevant dir
    
    * credentials
    
    * digital-workspace path
    
    * adding nginx for adw
    
    * lint
    
    * lint
    
    * refactor
    
    * typo
    
    * change the way adw is served
    
    * trigger another build
    
    * fix common issue
    
    * fixing stuff
    
    * more fixing
    
    * and some more.
    
    * fix
    
    * changing var location
    
    * changed location for var
    
    Co-authored-by: Lucian Suciu <lucian.suciu@ness.com>

---
+++ roles/adw/molecule/default/tests/test_adw.py
@@ -0,0 +1,16 @@
+"""Repo Tests"""
+import pytest
+from hamcrest import assert_that, contains_string
+
+# pylint: disable=redefined-outer-name
+@pytest.fixture()
+def AnsibleVars(host):
+    """Define AnsibleVars"""
+    common_vars = "../../../common/vars/main.yml name=common_vars"
+    ansible_vars = host.ansible("include_vars", common_vars)["ansible_facts"]["common_vars"]
+    return ansible_vars
+
+def test_digital_workspace_200(host, AnsibleVars):
+    "Check that /digital-workspace context is available and returns a HTTP 200 status code"
+    cmd = host.run("curl -iL --user admin:admin http://{}:8880/".format(AnsibleVars["adw_host"]))
+    assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
