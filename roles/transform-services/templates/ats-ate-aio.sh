4084129142c40dd7f7edae30c2e0f13cc4cde944
Andrei Ciubotaru <andrei.ciubotaru@ness.com>
2020-09-23 14:20:47 +0300

    Added some tests

--- roles/transform-services/templates/ats-ate-aio.sh
+++ roles/transform-services/templates/ats-ate-aio.sh
@@ -9,4 +9,4 @@ export JAVA_OPTS="${JAVA_OPTS} -DPDFRENDERER_EXE=${ACS_HOME}/alfresco-pdf-render
 export JAVA_OPTS="${JAVA_OPTS} -DLIBREOFFICE_HOME=${LIBREOFFICE_HOME}"
 export JAVA_OPTS="${JAVA_OPTS} -DIMAGEMAGICK_ROOT=${IMAGEMAGICK_HOME} -DIMAGEMAGICK_DYN=${IMAGEMAGICK_DYN} -DIMAGEMAGICK_EXE=${IMAGEMAGICK_EXE} -DIMAGEMAGICK_CONFIG=${IMAGEMAGICK_CONFIG} -DIMAGEMAGICK_CODERS=${IMAGEMAGICK_CODERS}"
 export JAVA_OPTS="${JAVA_OPTS} -DACTIVEMQ_URL=failover:(tcp://${ACTIVEMQ_HOST}:61616)?timeout=3000"
-${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar ${ATS_HOME}/alfresco-transform-core-aio-boot-*.jar > /var/log/alfresco/ats-ate-aio.log
+${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar ${ATS_HOME}/alfresco-transform-core-aio-boot-*.jar > {{ logs_folder }}/ats-ate-aio.log
