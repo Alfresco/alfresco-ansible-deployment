73a8dcb9ff08b194c0d11d1f3b3710cf695601d0
Andrei Ciubotaru <andrei.ciubotaru@ness.com>
2020-10-01 19:10:38 +0300

    OPSEXP-524: Repository breaks on the second converge (#24)
    Fixed some of the idempotence issues, fixed typos, made code more readable

--- roles/repository/templates/tomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME="{{ java_home }}"
-export PATH="${JAVA_HOME}/bin:${PATH}"
-
-export PATH=$PATH:{{ tomcat_instance }}/bin:$JAVA_HOME/bin
-export TOMCAT_NATIVE_LIBDIR={{ tomcat_instance }}/native-jni-lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$TOMCAT_NATIVE_LIBDIR
