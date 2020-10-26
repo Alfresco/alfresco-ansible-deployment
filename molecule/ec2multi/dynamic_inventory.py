d6d5f7398ad05e96dcf24a8cd6fcff89cb484013
Sergiu Vidrascu <vsergiu@hotmail.com>
2020-10-26 10:24:29 +0200

    OPSEXP-540 inventory setup and molecule update (#39)
    * initial commit [keep env]
    
    * split dependency and postgres with group vars
    
    * some changes
    
    * add all services in split mode
    
    * and more
    
    * move vars
    
    * and more
    
    * REMOVE DUPLICATION?
    
    * lint
    
    * add this also
    
    * updated travis build and added multi instance stage
    
    * Fix linters
    
    * change scenario name
    
    * fix paths
    
    * update machine types and temp folder
    
    * fix spot price
    
    * fix typo
    
    * fix typo on spot price
    
    * add spot price for single instance also
    
    * review comments
    
    * remove lucians branch changes
    
    * revert changes from lucian
    
    * revert changes from lucian
    
    * add additional playbook
    
    * update multimachine
    
    * update single
    
    * modify single playbook run and playbook
    
    * escape chars
    
    * fix escape
    
    * retry escaping
    
    * try again?
    
    * one more try
    
    * one more try
    
    * again
    
    * and again
    
    * another try
    
    * remove spacing
    
    * Hopefully last update
    
    * comment spot on single instance
    
    * fix target inventory for single machine
    
    * update single machine scripts
    
    * Update converge.yml
    
    Included postgres role in the converge.yml file. Postgres was removed as dependency and this will break the unit tests
    
    Co-authored-by: root <root@ip-172-31-74-224.ec2.internal>
    Co-authored-by: Lucian Suciu <lucian.suciu@ness.com>
    Co-authored-by: Sergiu-Alexandru Vidrascu <p3700456@L3700100984.ness.com>
    Co-authored-by: Andrei Ciubotaru <andrei.ciubotaru@ness.com>

---
+++ molecule/ec2multi/dynamic_inventory.py
@@ -0,0 +1,31 @@
+import os
+import re
+import yaml
+
+yamlfile= open(os.environ['MOLECULE_EPHEMERAL_DIRECTORY'] + "/instance_config.yml")
+parsed_yaml_file = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+new_inventory = open(os.environ['MOLECULE_EPHEMERAL_DIRECTORY'] + "/new_inventory.yml", "w")
+inventoryfile = {'all': {'children':{}}}
+
+def add_if_key_not_exist(dict_obj, key, value):
+    """ Add new key-value pair to dictionary only if
+    key does not exist in dictionary. """
+    if key not in dict_obj:
+        dict_obj.update({key: value})
+
+for item in parsed_yaml_file:
+    print(item)
+    groupname = re.sub(r'_.*', '', item['instance'])
+    add_if_key_not_exist(inventoryfile['all']['children'], groupname, {})
+    add_if_key_not_exist(inventoryfile['all']['children'][groupname], 'hosts', {})
+    add_if_key_not_exist(inventoryfile['all']['children'][groupname]['hosts'], item['instance'], {
+        'ansible_host': item['address'],
+        'ansible_private_key_file': item['identity_file'],
+        'ansible_ssh_common_args': "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no",
+        'ansible_user': 'centos',
+        'connection': 'ssh'
+    })
+
+yaml.dump(inventoryfile, new_inventory)
+new_inventory.close()
