c6a8786475bb37578ef8417d1001fb5bb570f5f2
Sergiu Vidrascu <sergiu.vidrascu@ness.com>
2020-11-05 09:27:01 +0200

    Update test_repository.py

--- roles/repository/molecule/default/tests/test_repository.py
+++ roles/repository/molecule/default/tests/test_repository.py
@@ -6,17 +6,18 @@ from hamcrest import assert_that, contains_string
 @pytest.fixture()
 def get_ansible_vars(host):
     """Define get_ansible_vars"""
-    java_role = "file=../../../java/vars/main.yml name=java_role"
-    common_vars = "file=../../../common/vars/main.yml name=common_vars"
-    common_defaults = "file=../../../common/defaults/main.yml name=common_defaults"
+   repository_role = "file=../../vars/main.yml name=repository_role"
     tomcat_role = "file=../../../roles/tomcat/vars/main.yml name=tomcat_role"
-    repository_role = "file=../../vars/main.yml name=repository_role"
-    ansible_vars = host.ansible("include_vars", java_role)["ansible_facts"]["java_role"]
+    java_role = "file=../../../roles/java/vars/main.yml name=java_role"
+    common_vars = "../../../common/vars/main.yml name=common_vars"
+    common_defaults = "../../../common/defaults/main.yml name=common_defaults"
+    common_hosts = "../../inventory_acs/group_vars/all.yml name=common_hosts"
+    ansible_vars = host.ansible("include_vars", tomcat_role)["ansible_facts"]["tomcat_role"]
+    ansible_vars.update(host.ansible("include_vars", repository_role)["ansible_facts"]["repository_role"])
     ansible_vars.update(host.ansible("include_vars", java_role)["ansible_facts"]["java_role"])
     ansible_vars.update(host.ansible("include_vars", common_vars)["ansible_facts"]["common_vars"])
+    ansible_vars.update(host.ansible("include_vars", common_hosts)["ansible_facts"]["common_hosts"])
     ansible_vars.update(host.ansible("include_vars", common_defaults)["ansible_facts"]["common_defaults"])
-    ansible_vars.update(host.ansible("include_vars", tomcat_role)["ansible_facts"]["tomcat_role"])
-    ansible_vars.update(host.ansible("include_vars", repository_role)["ansible_facts"]["repository_role"])
     return ansible_vars
 
 def test_alfresco_context_200(host, get_ansible_vars):
