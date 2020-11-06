21750ca67444ed8b6d3ce9a7762a2c1eb182b15c
Lucian Suciu <lucian.suciu@ness.com>
2020-11-06 08:55:48 +0200

    split inventory for multi also

--- molecule/ec2multi/dynamic_inventory.py
+++ molecule/ec2multi/dynamic_inventory.py
@@ -27,5 +27,16 @@ for item in parsed_yaml_file:
         'connection': 'ssh'
     })
 
+    if (groupname=='webservers'):
+        add_if_key_not_exist(inventoryfile['all']['children'], 'adw', {})
+        add_if_key_not_exist(inventoryfile['all']['children']['adw'], 'hosts', {})
+        add_if_key_not_exist(inventoryfile['all']['children']['adw']['hosts'], 'adw_1', {
+            'ansible_host': item['address'],
+            'ansible_private_key_file': item['identity_file'],
+            'ansible_ssh_common_args': "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no",
+            'ansible_user': 'centos',
+            'connection': 'ssh'
+        })
+
 yaml.dump(inventoryfile, new_inventory)
 new_inventory.close()
