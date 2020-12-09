19523d4d7bb12a03114bcc03457a0d16ce2ac483
Gavin Cornwell <gavin.cornwell@alfresco.com>
2020-12-09 15:35:58 +0000

    Consolidated release stage

--- scripts/generate-zip.sh
+++ scripts/generate-zip.sh
@@ -13,16 +13,18 @@ cd $SCRIPT_DIR/../dist
 VERSION=`cat ../VERSION`
 
 # create temporary folder with required files
+echo "Copying required files to temporary folder..."
 BUILD_FOLDER=alfresco-ansible-deployment-${VERSION}
 mkdir -p $BUILD_FOLDER
 echo "For more info on this project goto https://github.com/Alfresco/alfresco-ansible-deployment/blob/master/README.md" > $BUILD_FOLDER/README.txt
 cp -r ../group_vars $BUILD_FOLDER
 cp -r ../playbooks $BUILD_FOLDER
-cp -r ../roles $BUILD_FOLDER
+rsync -rvq --exclude molecule/ ../roles $BUILD_FOLDER
 cp ../inventory_* $BUILD_FOLDER
 cp ../LICENSE $BUILD_FOLDER
 cp ../VERSION $BUILD_FOLDER
 
 # generate ZIP
+echo "Generating ZIP file..."
 zip -r alfresco-ansible-deployment-${VERSION}.zip $BUILD_FOLDER
 rm -rf $BUILD_FOLDER
