ca25c02dfa6388803c4e2e52e2d0ebb7492ba67e
sergiu.vidrascu@ness.com <sergiu.vidrascu@ness.com>
2021-02-10 22:37:23 +0200

    fix linting

--- roles/activemq/molecule/default/tests/test_activemq.py
+++ roles/activemq/molecule/default/tests/test_activemq.py
@@ -54,3 +54,4 @@ def test_environment_jvm_opts(host, get_ansible_vars):
     pid = host.run("/opt/openjdk*/bin/jps -lV | grep activemq | awk '{print $1}'")
     process_map = host.run("/opt/openjdk*/bin/jhsdb jmap --heap --pid {}".format(pid.stdout))
     assert_that(process_map.stdout, contains_string("MaxHeapSize              = 943718400 (900.0MB)"))
+
