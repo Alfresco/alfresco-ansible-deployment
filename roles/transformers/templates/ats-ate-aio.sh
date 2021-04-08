71dbc39544d17feeba80b890c6385988333e3a71
Alexandre Chapellon <alexandre.chapellon@alfresco.com>
2021-04-08 12:29:57 +0200

    fix wrong port for sfs

--- roles/transformers/templates/ats-ate-aio.sh
+++ roles/transformers/templates/ats-ate-aio.sh
@@ -10,7 +10,7 @@ export JAVA_OPTS="${JAVA_OPTS} -DLIBREOFFICE_HOME=${LIBREOFFICE_HOME}"
 export JAVA_OPTS="${JAVA_OPTS} -DIMAGEMAGICK_ROOT=${IMAGEMAGICK_HOME} -DIMAGEMAGICK_DYN=${IMAGEMAGICK_DYN} -DIMAGEMAGICK_EXE=${IMAGEMAGICK_EXE} -DIMAGEMAGICK_CONFIG=${IMAGEMAGICK_CONFIG} -DIMAGEMAGICK_CODERS=${IMAGEMAGICK_CODERS}"
 {% if acs.edition == 'Enterprise' %}
 export JAVA_OPTS="${JAVA_OPTS} -DACTIVEMQ_URL=failover:(tcp://{{ activemq_host }}:{{ ports_cfg.activemq.openwire }})?timeout=3000"
-export JAVA_OPTS="${JAVA_OPTS} -DFILE_STORE_URL=http://${ATS_SHARED_FS_HOST}:{{ ports_cfg.trouter.http }}/alfresco/api/-default-/private/sfs/versions/1/file"
+export JAVA_OPTS="${JAVA_OPTS} -DFILE_STORE_URL=http://${ATS_SHARED_FS_HOST}:{{ ports_cfg.sfs.http }}/alfresco/api/-default-/private/sfs/versions/1/file"
 {% endif %}
 
 {% for key, value in tengine_environment.items() %}
