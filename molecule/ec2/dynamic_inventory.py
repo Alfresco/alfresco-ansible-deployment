27ff5446b47aaf4abd8412e5f8b6a688bea45ba4
Alexandre Chapellon <alexandre.chapellon@alfresco.com>
2021-05-01 16:46:23 +0200

    pylint 2.8.2 code style

--- molecule/ec2/dynamic_inventory.py
+++ molecule/ec2/dynamic_inventory.py
@@ -5,37 +5,34 @@ Generate inventory from Molecule scenario
 import os
 import yaml
 
-yamlfile= open(os.environ['MOLECULE_EPHEMERAL_DIRECTORY'] + "/instance_config.yml")
-parsed_yaml_file = yaml.load(yamlfile, Loader=yaml.FullLoader)
+with open(os.environ['MOLECULE_EPHEMERAL_DIRECTORY'] + "/instance_config.yml") as yamlfile:
+    parsed_yaml_file = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-old_inventory = open(os.path.dirname(os.path.abspath(__file__)) + "/../../inventory_ssh.yml", "r")
-parsed_old_inventory = yaml.load(old_inventory, Loader=yaml.FullLoader)
-new_inventory = open(os.path.dirname(os.path.abspath(__file__)) + "/../../inventory_ssh.yml", "w")
-old_inventory.close()
+with  open(os.path.dirname(os.path.abspath(__file__)) + "/../../inventory_ssh.yml", "r") as old_inventory:
+    parsed_old_inventory = yaml.load(old_inventory, Loader=yaml.FullLoader)
 
+with open(os.path.dirname(os.path.abspath(__file__)) + "/../../inventory_ssh.yml", "w") as new_inventory:
+    inventoryfile = {'all': {'children':{}}}
 
-inventoryfile = {'all': {'children':{}}}
+    def add_if_key_not_exist(dict_obj, key, value):
+        """ Add new key-value pair to dictionary only if
+        key does not exist in dictionary. """
+        if key not in dict_obj:
+            dict_obj.update({key: value})
 
-def add_if_key_not_exist(dict_obj, key, value):
-    """ Add new key-value pair to dictionary only if
-    key does not exist in dictionary. """
-    if key not in dict_obj:
-        dict_obj.update({key: value})
+    address = parsed_yaml_file[0]['address']
+    identity_file = parsed_yaml_file[0]['identity_file']
 
-address = parsed_yaml_file[0]['address']
-identity_file = parsed_yaml_file[0]['identity_file']
+    for item in parsed_old_inventory['all']['children']:
+        print(item)
+        groupname = item + "_1"
+        add_if_key_not_exist(inventoryfile['all']['children'], item, {})
+        add_if_key_not_exist(inventoryfile['all']['children'][item], 'hosts', {})
+        add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'], groupname, {})
+        add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_host', address)
+        add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_private_key_file', identity_file)
+        add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_ssh_common_args', "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no")
+        add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_user', 'centos')
+        add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'connection', 'ssh')
 
-for item in parsed_old_inventory['all']['children']:
-    print(item)
-    groupname = item + "_1"
-    add_if_key_not_exist(inventoryfile['all']['children'], item, {})
-    add_if_key_not_exist(inventoryfile['all']['children'][item], 'hosts', {})
-    add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'], groupname, {})
-    add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_host', address)
-    add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_private_key_file', identity_file)
-    add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_ssh_common_args', "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no")
-    add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'ansible_user', 'centos')
-    add_if_key_not_exist(inventoryfile['all']['children'][item]['hosts'][groupname], 'connection', 'ssh')
-
-yaml.dump(inventoryfile, new_inventory)
-new_inventory.close()
+    yaml.dump(inventoryfile, new_inventory)
--- molecule/ec2multi/dynamic_inventory.py
+++ molecule/ec2multi/dynamic_inventory.py
@@ -6,35 +6,24 @@ import os
 import re
 import yaml
 
-yamlfile= open(os.environ['MOLECULE_EPHEMERAL_DIRECTORY'] + "/instance_config.yml")
-parsed_yaml_file = yaml.load(yamlfile, Loader=yaml.FullLoader)
+with open(os.environ['MOLECULE_EPHEMERAL_DIRECTORY'] + "/instance_config.yml") as yamlfile:
+    parsed_yaml_file = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-new_inventory = open(os.path.dirname(os.path.abspath(__file__)) + "/../../inventory_ssh.yml", "w")
-inventoryfile = {'all': {'children':{}}}
+with open(os.path.dirname(os.path.abspath(__file__)) + "/../../inventory_ssh.yml", "w") as new_inventory:
+    inventoryfile = {'all': {'children':{}}}
 
-def add_if_key_not_exist(dict_obj, key, value):
-    """ Add new key-value pair to dictionary only if
-    key does not exist in dictionary. """
-    if key not in dict_obj:
-        dict_obj.update({key: value})
+    def add_if_key_not_exist(dict_obj, key, value):
+        """ Add new key-value pair to dictionary only if
+        key does not exist in dictionary. """
+        if key not in dict_obj:
+            dict_obj.update({key: value})
 
-for item in parsed_yaml_file:
-    print(item)
-    groupname = re.sub(r'_.*', '', item['instance'])
-    add_if_key_not_exist(inventoryfile['all']['children'], groupname, {})
-    add_if_key_not_exist(inventoryfile['all']['children'][groupname], 'hosts', {})
-    add_if_key_not_exist(inventoryfile['all']['children'][groupname]['hosts'], item['instance'], {
-        'ansible_host': item['address'],
-        'ansible_private_key_file': item['identity_file'],
-        'ansible_ssh_common_args': "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no",
-        'ansible_user': 'centos',
-        'connection': 'ssh'
-    })
-
-    if groupname=='nginx':
-        add_if_key_not_exist(inventoryfile['all']['children'], 'adw', {})
-        add_if_key_not_exist(inventoryfile['all']['children']['adw'], 'hosts', {})
-        add_if_key_not_exist(inventoryfile['all']['children']['adw']['hosts'], 'adw_1', {
+    for item in parsed_yaml_file:
+        print(item)
+        groupname = re.sub(r'_.*', '', item['instance'])
+        add_if_key_not_exist(inventoryfile['all']['children'], groupname, {})
+        add_if_key_not_exist(inventoryfile['all']['children'][groupname], 'hosts', {})
+        add_if_key_not_exist(inventoryfile['all']['children'][groupname]['hosts'], item['instance'], {
             'ansible_host': item['address'],
             'ansible_private_key_file': item['identity_file'],
             'ansible_ssh_common_args': "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no",
@@ -42,5 +31,15 @@ for item in parsed_yaml_file:
             'connection': 'ssh'
         })
 
-yaml.dump(inventoryfile, new_inventory)
-new_inventory.close()
+        if groupname=='nginx':
+            add_if_key_not_exist(inventoryfile['all']['children'], 'adw', {})
+            add_if_key_not_exist(inventoryfile['all']['children']['adw'], 'hosts', {})
+            add_if_key_not_exist(inventoryfile['all']['children']['adw']['hosts'], 'adw_1', {
+                'ansible_host': item['address'],
+                'ansible_private_key_file': item['identity_file'],
+                'ansible_ssh_common_args': "-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s -o ForwardX11=no -o LogLevel=ERROR -o IdentitiesOnly=yes -o StrictHostKeyChecking=no",
+                'ansible_user': 'centos',
+                'connection': 'ssh'
+            })
+
+    yaml.dump(inventoryfile, new_inventory)
