bad7924eba4c92528016df9b2d8acb9af579e364
Alexandre Chapellon <alexandre.chapellon@alfresco.com>
2021-10-26 11:30:44 +0200

    amend zip release script

--- scripts/generate-zip.sh
+++ scripts/generate-zip.sh
@@ -18,12 +18,13 @@ mkdir -p $BUILD_FOLDER
 echo "You can find full documentation on how to use the playbook for Enterprise deployments here https://docs.alfresco.com/content-services/latest/install/ansible and for Community deployments here https://docs.alfresco.com/content-services/community/install/ansible" > $BUILD_FOLDER/README.txt
 cp -r ../group_vars $BUILD_FOLDER
 cp -r ../playbooks $BUILD_FOLDER
+cp -r ../docs $BUILD_FOLDER
 rsync -rvq --exclude molecule/ ../roles $BUILD_FOLDER
 cp ../inventory_* $BUILD_FOLDER
+cp ../*.md $BUILD_FOLDER
 cp ../LICENSE $BUILD_FOLDER
 cp ../VERSION $BUILD_FOLDER
-cp ../6.2.N-extra-vars.yml $BUILD_FOLDER
-cp ../community-extra-vars.yml $BUILD_FOLDER
+cp ../*-extra-vars.yml $BUILD_FOLDER
 cp -r ../configuration_files $BUILD_FOLDER
 
 # generate ZIP
