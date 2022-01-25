805f8041f3ca5a784867f0c21c3f90597e6d32e5
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-01-25 15:35:33 +0100

    merge

--- scripts/integration/script.sh
+++ scripts/integration/script.sh
@@ -1,4 +1,22 @@
 #!/bin/bash -e
+openssl aes-256-cbc -K $encrypted_6c8b9ee48a27_key -iv $encrypted_6c8b9ee48a27_iv \
+    -in alfresco-ansible.pem.enc -out /tmp/dbp-ansible -d
+
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/dbp-ansible
+ssh-add /tmp/dbp-ansible
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+
+export PATH=~/bin:$PATH
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION='us-east-1'
+
+git clone --depth 1 --branch $DTAS_VERSION https://$GITHUB_TOKEN@github.com/Alfresco/alfresco-deployment-test-automation-scripts.git dtas
+
 aws ec2 run-instances --image-id ami-0affd4508a5d2481b \
     --count 1 --instance-type t3.large --key-name dbp-ansible \
     --security-group-ids sg-099a05787b0a322c3 --subnet-id subnet-06ffe524598660d99 \
@@ -11,9 +29,9 @@ export INSTANCE_ID=$(aws ec2 describe-instances --filters Name=tag-key,Values=lo
 
 ssh-keyscan $PUBLIC_IP >> ~/.ssh/known_hosts
 
-scripts/generate-zip.sh
+./scripts/generate-zip.sh
 export VERSION=$(cat VERSION)
-scp dist/alfresco-ansible-deployment-${VERSION}.zip centos@${PUBLIC_IP}:/home/centos/
+scp ./dist/alfresco-ansible-deployment-${VERSION}.zip centos@${PUBLIC_IP}:/home/centos/
 ssh centos@${PUBLIC_IP} "sudo yum install -y -q unzip python3 python-virtualenv"
 ssh centos@${PUBLIC_IP} "mkdir ~/.pythonvenv && virtualenv -p /usr/bin/python3 ~/.pythonvenv/ansible-${ANSIBLE_VERSION}"
 ssh centos@${PUBLIC_IP} "source ~/.pythonvenv/ansible-${ANSIBLE_VERSION}/bin/activate && pip install --upgrade pip && pip install ansible==${ANSIBLE_VERSION}"
