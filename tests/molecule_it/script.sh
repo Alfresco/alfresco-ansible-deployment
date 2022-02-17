be27ba0f895fd6d3d590be88e4d354a7f4d4f88a
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-02-17 16:31:21 +0100

    make shellcheck happy

--- tests/molecule_it/script.sh
+++ tests/molecule_it/script.sh
@@ -5,11 +5,11 @@ if [ -n "$MOLECULE_IT_SCENARIO" ]; then
     if [ -n "$MOLECULE_IT_CONFIG" ]; then
         EXTRA_CONFIG="-e $MOLECULE_IT_PATH/$MOLECULE_IT_CONFIG"
     fi
-    if [ $1 == 'destroy' ]; then
-        molecule $EXTRA_CONFIG destroy -s $MOLECULE_IT_SCENARIO
-    elif [ $1 == 'verify' ]; then
-        molecule $EXTRA_CONFIG converge -s $MOLECULE_IT_SCENARIO || exit 1
-        molecule $EXTRA_CONFIG verify -s $MOLECULE_IT_SCENARIO
+    if [ "$1" == 'destroy' ]; then
+        molecule "$EXTRA_CONFIG" destroy -s "$MOLECULE_IT_SCENARIO"
+    elif [ "$1" == 'verify' ]; then
+        molecule "$EXTRA_CONFIG" converge -s "$MOLECULE_IT_SCENARIO" || exit 1
+        molecule "$EXTRA_CONFIG" verify -s "$MOLECULE_IT_SCENARIO"
     else
         echo "$1: invalid command"
         exit 1
