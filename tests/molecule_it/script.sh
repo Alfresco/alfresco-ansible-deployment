8e7ca461d557a4f48b250a81782821ee4989791f
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-02-23 10:36:05 +0100

    OPSEXP-1232 multimachine ec2 molecule tests (#245)
    Co-authored-by: Alex Chapellon <alexandre.chapellon@alfresco.com>

---
+++ tests/molecule_it/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+if [ -n "$MOLECULE_IT_SCENARIO" ]; then
+    EXTRA_CONFIG=""
+    MOLECULE_IT_PATH="molecule/$MOLECULE_IT_SCENARIO"
+    if [ -n "$MOLECULE_IT_CONFIG" ]; then
+        EXTRA_CONFIG="-e $MOLECULE_IT_PATH/$MOLECULE_IT_CONFIG"
+    fi
+    if [ "$1" == 'destroy' ]; then
+        # shellcheck disable=SC2086
+        molecule $EXTRA_CONFIG destroy -s "$MOLECULE_IT_SCENARIO"
+    elif [ "$1" == 'verify' ]; then
+        # shellcheck disable=SC2086
+        molecule $EXTRA_CONFIG converge -s "$MOLECULE_IT_SCENARIO" || exit 1
+        # shellcheck disable=SC2086
+        molecule $EXTRA_CONFIG verify -s "$MOLECULE_IT_SCENARIO"
+    else
+        echo "$1: invalid command"
+        exit 1
+    fi
+else
+    echo "no molecule it scenario is set, doing nothing"
+fi
