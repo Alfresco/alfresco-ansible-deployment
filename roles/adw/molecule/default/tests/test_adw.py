100a40e443df0bf601f5b80d9e11f43eb5a3632f
Alex Chapellon <alexandre.chapellon@alfresco.com>
2022-04-19 15:40:51 +0200

    OPSEXP-1404: controled usage of become plugin in adw role (#323)
    * move default vars to defaults file
    
    * rename missleading variable request_uri to adw_context
    
    * refactor adw role for better become usage
    
    * make adw role tests non priviledged

--- roles/adw/molecule/default/tests/test_adw.py
+++ roles/adw/molecule/default/tests/test_adw.py
@@ -1,23 +1,10 @@
 """ADW Tests"""
 import os
-import pytest
 from hamcrest import assert_that, contains_string
-
 
 test_host = os.environ.get('TEST_HOST')
 
-
-# pylint: disable=redefined-outer-name
-@pytest.fixture(scope="module")
-def get_ansible_vars(host):
-    """Define get_ansible_vars"""
-    common_vars = "file=../common/vars/main.yml name=common_vars"
-    common_hosts = "file=../common/defaults/main.yml name=common_hosts"
-    ansible_vars = host.ansible("include_vars", common_vars)["ansible_facts"]["common_vars"]
-    ansible_vars.update(host.ansible("include_vars", common_hosts)["ansible_facts"]["common_hosts"])
-    return ansible_vars
-
-def test_digital_workspace_200(host, get_ansible_vars):
+def test_digital_workspace_200(host):
     "Check that ADW is available and returns a HTTP 200 status code"
     cmd = host.run("curl -iL --user admin:admin http://{}:8880/".format(test_host))
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
