0536e612e720121e15107262335040b734f4cff2
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-05-02 16:54:26 +0200

    fix tests

--- roles/java/molecule/default/tests/test_java.py
+++ roles/java/molecule/default/tests/test_java.py
@@ -19,19 +19,19 @@ def get_ansible_vars(host):
 
 def test_java_exists(host, get_ansible_vars):
     """Check that java executable exists"""
-    java_version = get_ansible_vars["dependencies_version"]["jdk"]
+    java_version = get_ansible_vars["dependencies_version"]["java"]
     java_bin = host.file("/opt/openjdk-{}/bin/java".format(java_version))
     assert_that(java_bin.exists, True)
 
 def test_java_version(host, get_ansible_vars):
     """Check that java version is correct"""
-    java_version = get_ansible_vars["dependencies_version"]["jdk"]
+    java_version = get_ansible_vars["dependencies_version"]["java"]
     cmd = host.run("/opt/openjdk-{}/bin/java -version".format(java_version))
-    assert_that(cmd.stderr, contains_string(get_ansible_vars["dependencies_version"]["jdk"]))
+    assert_that(cmd.stderr, contains_string(java_version))
 
 def test_java_home(host, get_ansible_vars):
     """Check that JAVA_HOME is environment variable is set"""
-    java_version = get_ansible_vars["dependencies_version"]["jdk"]
+    java_version = get_ansible_vars["dependencies_version"]["java"]
     with host.sudo():
         cmd = host.run(". {}/setenv.sh && echo $JAVA_HOME".format(get_ansible_vars["config_folder"]))
     assert_that(cmd.stdout, contains_string("/opt/openjdk-{}".format(java_version)))
