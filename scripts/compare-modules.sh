781dfc08e0780412c80a2081afd078f42b7daef0
Kacper Klassa <kacper.klassa@hyland.com>
2022-08-01 15:56:12 +0200

    change compare-modules.sh to only check specified modules

--- scripts/compare-modules.sh
+++ scripts/compare-modules.sh
@@ -2,26 +2,35 @@
 
 PIP_MODULES=$(pip3 freeze)
 
-while read -r LINE; do
+declare -A module_versions
 
-  IFS='==' read -ra REQ_MODULE <<< "$LINE"
-  REQ_MODULE_NAME="${REQ_MODULE[0]}"
-  IFS='.' read -ra REQ_MODULE_VERSION <<< "${REQ_MODULE[2]}"
-  REQ_MODULE_VERSION_FULL="${REQ_MODULE_VERSION[0]}.${REQ_MODULE_VERSION[1]}.${REQ_MODULE_VERSION[2]}"
+module_versions=(
+  ["ansible-core"]="2.12.7"
+  ["Jinja2"]="3.1.2"
+)
 
-  PIP_MODULE=$(echo $PIP_MODULES | tr ' ' '\n' | grep ${REQ_MODULE_NAME}==)
-  if [ -z $PIP_MODULE ]; then 
+for key in "${!module_versions[@]}"; do
+  
+  REQ_MODULE_NAME="$key"
+  REQ_MODULE_VERSION_FULL="${module_versions[$key]}"
+  IFS='.' read -ra REQ_MODULE_VERSION <<< "$REQ_MODULE_VERSION_FULL"
+
+  PIP_MODULE=$(echo "$PIP_MODULES" | grep ${REQ_MODULE_NAME}==)
+  if [ -z "$PIP_MODULE" ]; then 
     echo "${REQ_MODULE_NAME} not found";
     exit 1 
   fi
 
   IFS='==' read -ra PIP_MODULE <<< "$PIP_MODULE"
   PIP_MODULE_NAME="${PIP_MODULE[0]}"
-  IFS='.' read -ra PIP_MODULE_VERSION <<< "${PIP_MODULE[2]}"
+  PIP_MODULE_VERSION_FULL="${PIP_MODULE[2]}"
+  IFS='.' read -ra PIP_MODULE_VERSION <<< $PIP_MODULE_VERSION_FULL
   if [[ $((${PIP_MODULE_VERSION[0]})) -lt $((${REQ_MODULE_VERSION[0]})) ]] ||
      [[ $((${PIP_MODULE_VERSION[1]})) -lt $((${REQ_MODULE_VERSION[1]})) ]] ||
      [[ ${REQ_MODULE_VERSION[2]} && $((${PIP_MODULE_VERSION[2]})) -lt $((${REQ_MODULE_VERSION[2]})) ]]; then
-    echo "Unsupported $PIP_MODULE_NAME version. Upgrade to at least $REQ_MODULE_VERSION_FULL to avoid errors during deployment"
+    a="Unsupported $PIP_MODULE_NAME version: $PIP_MODULE_VERSION_FULL."
+    b="Upgrade to at least $REQ_MODULE_VERSION_FULL to avoid errors during deployment"
+    echo $a $b
     exit 2
   fi
-done < ../requirements.txt
+done
