94d1691ec17ac392d4a42de166768c886e53b88f
piraartur <Artur.Pira@hyland.com>
2022-08-01 14:31:12 +0200

    removed export path command, added command to install Pipfile

--- install_pipenv.sh
+++ install_pipenv.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-pwd
-ls -a
-cat Pipfile
-apt-get update
-apt-get install python3.10 -y
-apt-get install python3-pip -y
-pip3 install -U urllib3 requests
-pip3 install --user pipenv
+sudo apt-get update
+sudo apt-get install python3.9 -y
+sudo apt-get install python3-pip -y
+sudo pip3 install -U urllib3 requests
+sudo pip3 install --user pipenv
 export PATH="$HOME/.local/bin:$PATH"
-pipenv install Pipfile
+pipenv shell
+pipenv install
