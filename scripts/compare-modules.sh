e1897e99771f5d7abe9a8136e2882799c6ef291b
Kacper Klassa <kacper.klassa@hyland.com>
2022-08-01 16:06:47 +0200

    improvements to bash script suggested by Codacy Static Code Analysys

--- scripts/compare-modules.sh
+++ scripts/compare-modules.sh
@@ -15,7 +15,7 @@ for key in "${!module_versions[@]}"; do
   REQ_MODULE_VERSION_FULL="${module_versions[$key]}"
   IFS='.' read -ra REQ_MODULE_VERSION <<< "$REQ_MODULE_VERSION_FULL"
 
-  PIP_MODULE=$(echo "$PIP_MODULES" | grep ${REQ_MODULE_NAME}==)
+  PIP_MODULE=$(echo "$PIP_MODULES" | grep "${REQ_MODULE_NAME}"==)
   if [ -z "$PIP_MODULE" ]; then 
     echo "${REQ_MODULE_NAME} not found";
     exit 1 
@@ -24,13 +24,13 @@ for key in "${!module_versions[@]}"; do
   IFS='==' read -ra PIP_MODULE <<< "$PIP_MODULE"
   PIP_MODULE_NAME="${PIP_MODULE[0]}"
   PIP_MODULE_VERSION_FULL="${PIP_MODULE[2]}"
-  IFS='.' read -ra PIP_MODULE_VERSION <<< $PIP_MODULE_VERSION_FULL
-  if [[ $((${PIP_MODULE_VERSION[0]})) -lt $((${REQ_MODULE_VERSION[0]})) ]] ||
-     [[ $((${PIP_MODULE_VERSION[1]})) -lt $((${REQ_MODULE_VERSION[1]})) ]] ||
-     [[ ${REQ_MODULE_VERSION[2]} && $((${PIP_MODULE_VERSION[2]})) -lt $((${REQ_MODULE_VERSION[2]})) ]]; then
+  IFS='.' read -ra PIP_MODULE_VERSION <<< "$PIP_MODULE_VERSION_FULL"
+  if [[ $((PIP_MODULE_VERSION[0])) -lt $((REQ_MODULE_VERSION[0])) ]] ||
+     [[ $((PIP_MODULE_VERSION[1])) -lt $((REQ_MODULE_VERSION[1])) ]] ||
+     [[ ${REQ_MODULE_VERSION[2]} && $((PIP_MODULE_VERSION[2])) -lt $((REQ_MODULE_VERSION[2])) ]]; then
     a="Unsupported $PIP_MODULE_NAME version: $PIP_MODULE_VERSION_FULL."
     b="Upgrade to at least $REQ_MODULE_VERSION_FULL to avoid errors during deployment"
-    echo $a $b
+    echo "$a" "$b"
     exit 2
   fi
 done
