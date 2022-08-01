5f7bd3a33e9ca2ce81525036b06e504cbf402c71
piraartur <Artur.Pira@hyland.com>
2022-08-01 10:31:10 +0200

    updated .sh script to add pip and pipenv to PATH

--- install_pipenv.sh
+++ install_pipenv.sh
@@ -2,6 +2,11 @@
 apt-get update
 apt-get install python3.10 -y
 apt-get install python3-pip -y
+pip3 install -U urllib3 requests
 pip3 install --user pipenv
-pipenv install molecule
+echo -e 'export PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin"' >> $HOME/.bash_profile
+export PATH="$HOME/.local/bin:$PATH"
+cat Pipfile
+pwd
 pipenv install Pipfile
+echo test4
