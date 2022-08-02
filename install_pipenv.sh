d77d39e35a9f84e9b05229dff03d35f8cd83c841
piraartur <Artur.Pira@hyland.com>
2022-08-02 16:30:50 +0200

    fixed No module named apt_pkg error

--- install_pipenv.sh
+++ install_pipenv.sh
@@ -7,5 +7,7 @@ apt-get install python3-pip -y
 pip3 install -U urllib3 requests
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+update-alternatives  --set python3  /usr/bin/python3.9
 pip3 install pipenv
+pipenv install
 export PATH="$HOME/.local/bin:$PATH"
