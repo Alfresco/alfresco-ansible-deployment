446b388217dffa0f19a8f08c27f5acd1d00b0f5b
piraartur <Artur.Pira@hyland.com>
2022-08-02 12:28:10 +0200

    set default python3 version on vagrant to 3.9.13

--- install_pipenv.sh
+++ install_pipenv.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install python3.9 -y
-sudo apt-get install python3-pip -y
-sudo pip3 install -U urllib3 requests
-sudo pip3 install --user pipenv
+sudo su
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install python3.9 -y
+apt-get install python3-pip -y
+pip3 install -U urllib3 requests
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+pip3 install pipenv
 export PATH="$HOME/.local/bin:$PATH"
-pipenv shell
-pipenv install
