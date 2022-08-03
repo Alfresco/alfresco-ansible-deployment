21f0cf08aaef1d9e589a6125aac19b83ef63142c
piraartur <Artur.Pira@hyland.com>
2022-08-03 12:58:50 +0200

    solved No module named apt_pkg

--- install_pipenv.sh
+++ install_pipenv.sh
@@ -2,12 +2,16 @@
 sudo su
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update
+
 apt-get install python3.9 -y
 apt-get install python3-pip -y
-pip3 install -U urllib3 requests
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
 update-alternatives  --set python3  /usr/bin/python3.9
+sudo apt remove python3-apt
+sudo apt autoremove
+sudo apt autoclean
+sudo apt install python3-apt
 pip3 install pipenv
 pipenv install
 export PATH="$HOME/.local/bin:$PATH"
