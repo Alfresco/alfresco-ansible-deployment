640bd54b5d78fce401d760d5693d5d4425ecbde1
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-08-10 17:04:23 +0200

    Simplify and rename vagrant provision script

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure("2") do |config|
-  config.vm.provision "shell", path: "install_pipenv.sh"
+  config.vm.provision "shell", path: "./scripts/vagrant_provision.sh"
   config.vm.box = "ubuntu/focal64"
   config.vm.network "private_network", ip: "192.168.56.100"
   config.vm.network "forwarded_port", guest: 80, host: 80
@@ -7,5 +7,4 @@ Vagrant.configure("2") do |config|
     v.memory = 10240
     v.cpus = 4
   end
-  
 end
---
+++ scripts/vagrant_provision.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3.9 python3.9-dev python3.9-distutils
+curl -s -f https://bootstrap.pypa.io/get-pip.py | python3.9
+python3.9 -m pip install pipenv
+cd /vagrant
+python3.9 -m pipenv install --deploy
+python3.9 -m pipenv run ansible-galaxy install -r requirements.yml
+python3.9 -m pipenv run ansible-playbook -i inventory_local.yml playbooks/acs.yml
