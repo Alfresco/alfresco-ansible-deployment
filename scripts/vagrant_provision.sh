2d890f81371fdfdec82fcd3ffc290c57b310a654
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-08-11 16:09:56 +0200

    simplify vagrant with python 3.8

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -e
-add-apt-repository ppa:deadsnakes/ppa
-apt-get update
-apt-get install -y python3.9 python3.9-dev python3.9-distutils
-curl -s -f https://bootstrap.pypa.io/get-pip.py | python3.9
-python3.9 -m pip install pipenv
+curl https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python3
 cd /vagrant
-python3.9 -m pipenv install --deploy
-python3.9 -m pipenv run ansible-galaxy install -r requirements.yml
-python3.9 -m pipenv run ansible-playbook -i inventory_local.yml -e "autogen_unsecure_secrets=true" playbooks/acs.yml
+python3 -m pipenv install --deploy
+python3 -m pipenv run ansible-galaxy install -r requirements.yml
+python3 -m pipenv run ansible-playbook -i inventory_local.yml -e "autogen_unsecure_secrets=true" playbooks/acs.yml
