58c97b7a43b77c1fb7b0806b113233f0ac46d917
piraartur <Artur.Pira@hyland.com>
2022-09-12 10:13:11 +0200

    created python script to check OS version and distribution

---
+++ scripts/check-os.py
@@ -0,0 +1,27 @@
+import platform
+
+supported_distribution = ["RHEL", "CentOS", "Ubuntu"]
+supported_versions_RHEL = ["8.4", "8.2", "7.7", "7.6"]
+supported_versions_CentOS = ["7 x64",]
+supported_versions_ubuntu = ["20.04", "18.04"]
+
+distro = platform.release()
+version = platform.version()
+if distro in supported_distribution:
+    temp = supported_distribution[distro]
+    if temp == 0:
+        if version[0:3] not in supported_versions_RHEL:
+            print("You are on not supported RHEL version")
+        else:
+            pass 
+    elif temp == 1:
+        if version[0:5] not in supported_versions_CentOS:
+            print("You are on not supported CentOS version")
+        else: 
+            pass
+    elif temp == 2:
+        if version[0:6] not in supported_versions_ubuntu:
+            print("You are on not supported Ubuntu version")
+else: 
+    print("You are on not supported OS distribution!")
+
