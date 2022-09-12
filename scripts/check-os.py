ade2c699e88b3cc2f3714319642941929756e9bb
piraartur <Artur.Pira@hyland.com>
2022-09-12 10:34:21 +0200

    fixed precommit bugs

--- scripts/check-os.py
+++ scripts/check-os.py
@@ -1,8 +1,8 @@
 import platform
 
 supported_distribution = ["RHEL", "CentOS", "Ubuntu"]
-supported_versions_RHEL = ["8.4", "8.2", "7.7", "7.6"]
-supported_versions_CentOS = ["7 x64",]
+supported_versions_rhel = ["8.4", "8.2", "7.7", "7.6"]
+supported_versions_centos = ["7 x64",]
 supported_versions_ubuntu = ["20.04", "18.04"]
 
 distro = platform.release()
@@ -10,12 +10,12 @@ version = platform.version()
 if distro in supported_distribution:
     temp = supported_distribution[distro]
     if temp == 0:
-        if version[0:3] not in supported_versions_RHEL:
+        if version[0:3] not in supported_versions_rhel:
             print("You are on not supported RHEL version")
         else:
             pass 
     elif temp == 1:
-        if version[0:5] not in supported_versions_CentOS:
+        if version[0:5] not in supported_versions_centos:
             print("You are on not supported CentOS version")
         else: 
             pass
