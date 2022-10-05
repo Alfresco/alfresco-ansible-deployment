e4875fe19b90079004d3a8a4625185eb80604774
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-10-05 11:48:49 +0200

    fix command not found

--- roles/transformers/molecule/default/tests/test_transform.py
+++ roles/transformers/molecule/default/tests/test_transform.py
@@ -66,5 +66,5 @@ def test_libreoffice_install(host):
     assert_that(p.stdout, contains_string("LibreOffice 7.2.5.1 6d497ff5e83a906a307eb25cce314d40c0b8624f"))
 
 def test_no_ghostscript(host):
-    host.run_expect([1], "command -v gs")
-    host.run_expect([1], "command -v ghostscript")
+    host.run_expect([1], "which gs")
+    host.run_expect([1], "which ghostscript")
