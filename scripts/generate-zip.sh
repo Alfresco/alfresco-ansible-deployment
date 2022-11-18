c29cfe5b57a2fad41da292dc5c43344a43dfef65
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-11-18 15:52:24 +0100

    better packaging script

--- scripts/generate-zip.sh
+++ scripts/generate-zip.sh
@@ -1,32 +1,48 @@
 #!/bin/bash -e
 
-# find location of the script
+if [ -z "$VERSION" ]; then
+    echo "VERSION must be set"
+    exit 1
+fi
+
+# variables
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_ROOT_DIR="${SCRIPT_DIR}/.."
+ARTIFACT_DIR_NAME="alfresco-ansible-deployment-${VERSION}"
+BUILD_DIR="${REPO_ROOT_DIR}/dist/${ARTIFACT_DIR_NAME}"
+ARTIFACT_NAME="${ARTIFACT_DIR_NAME}.zip"
 
-# create dist folder
-mkdir -p "$SCRIPT_DIR/../dist"
-cd "$SCRIPT_DIR/../dist"
+echo "create distribution folder"
+mkdir -p "$BUILD_DIR"
 
-# create temporary folder with required files
-echo "Copying required files to temporary folder..."
-BUILD_FOLDER="alfresco-ansible-deployment-${VERSION}"
-mkdir -p "$BUILD_FOLDER"
-echo "You can find full documentation on how to use the playbook for Enterprise deployments here https://docs.alfresco.com/content-services/latest/install/ansible and for Community deployments here https://docs.alfresco.com/content-services/community/install/ansible" > "$BUILD_FOLDER"/README.txt
-cp -r ../group_vars "$BUILD_FOLDER"
-cp -r ../playbooks "$BUILD_FOLDER"
-cp -r ../docs "$BUILD_FOLDER"
-rsync -rvq --exclude molecule/ ../roles "$BUILD_FOLDER"
-cp ../inventory_* "$BUILD_FOLDER"
-cp ../*.md "$BUILD_FOLDER"
-cp ../LICENSE "$BUILD_FOLDER"
-cp ../*-extra-vars.yml "$BUILD_FOLDER"
-cp -r ../configuration_files "$BUILD_FOLDER"
-cp ../Vagrantfile ../requirements.{txt,yml} "$BUILD_FOLDER"
+cd "$REPO_ROOT_DIR"
 
-# generate ZIP
+echo "Adding main playbook resources"
+echo "You can find full documentation on how to use the playbook for Enterprise deployments here https://docs.alfresco.com/content-services/latest/install/ansible and for Community deployments here https://docs.alfresco.com/content-services/community/install/ansible" > "$BUILD_DIR/README.txt"
+cp -r configuration_files "$BUILD_DIR"
+cp -r docs "$BUILD_DIR"
+cp -r group_vars "$BUILD_DIR"
+cp -r playbooks "$BUILD_DIR"
+rsync -rvq --exclude molecule/ roles "$BUILD_DIR"
+cp inventory_*.yml "$BUILD_DIR"
+cp ./*.md "$BUILD_DIR"
+cp LICENSE "$BUILD_DIR"
+cp ./*-extra-vars.yml "$BUILD_DIR"
+
+echo "Adding vagrant support"
+cp Vagrantfile "$BUILD_DIR"
+mkdir "$BUILD_DIR/scripts"
+cp scripts/vagrant_provision.sh "$BUILD_DIR/scripts"
+
+echo "Adding Ansible Galaxy support"
+cp requirements.yml "$BUILD_DIR"
+
+echo "Adding Pipenv support"
+cp Pipfile Pipfile.lock "$BUILD_DIR"
+
+echo ""
 echo "Generating ZIP file..."
-zip -r "alfresco-ansible-deployment-${VERSION}.zip" "$BUILD_FOLDER"
+cd "$REPO_ROOT_DIR/dist"
+zip -r "$ARTIFACT_NAME" "$ARTIFACT_DIR_NAME"
 
-# cleanup
-echo "Removing temporary folder..."
-rm -rf "$BUILD_FOLDER"
+echo "Done"
