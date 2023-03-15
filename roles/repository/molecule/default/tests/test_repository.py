a9ec942f1ebac2d89fc444a62a96ce31db39474f
Alex Chapellon <alexandre.chapellon@alfresco.com>
2023-03-15 14:21:15 +0100

    OPSEXP-2036: fix CORS & CSRF config (#558)

--- roles/repository/molecule/default/tests/test_repository.py
+++ roles/repository/molecule/default/tests/test_repository.py
@@ -4,6 +4,7 @@ import time
 import string
 import random
 import json
+import defusedxml.ElementTree as xmlreader
 import pytest
 from hamcrest import assert_that, contains_string, has_length
 
@@ -26,6 +27,7 @@ def get_ansible_vars(host):
     ansible_vars.update(host.ansible("include_vars", tomcat_role)["ansible_facts"]["tomcat_role"])
     ansible_vars.update(host.ansible("include_vars", repository_role)["ansible_facts"]["repository_role"])
     return ansible_vars
+
 
 def brute_lock_admin(host):
     """
@@ -63,16 +65,19 @@ def test_bruteforce_mitigation(host):
                    login_api_endpoint, api_headers, login_payload))
     assert_that(json.loads(cmd.stdout)['entry']['userId'] == 'admin')
 
+
 def test_repo_service_is_running_and_enabled(host):
     """Check repository service"""
     repository = host.service("alfresco-content.service")
     assert_that(repository.is_running)
     assert_that(repository.is_enabled)
 
+
 def test_alfresco_log_exists(host):
     "Check that alfresco.log exists in /var/log/alfresco"
     with host.sudo():
         assert_that(host.file("/var/log/alfresco/alfresco.log").exists)
+
 
 def test_alfresco_context_200(host):
     "Check that /alfresco context is available and returns a HTTP 200 status code"
@@ -80,6 +85,7 @@ def test_alfresco_context_200(host):
         cmd = host.run("curl -iL --user admin:admin --connect-timeout 5 http://{}:8080/alfresco".format(test_host))
     assert_that(cmd.stdout, contains_string("Welcome to Alfresco"), test_host)
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
+
 
 def test_alfresco_api(host):
     "Check the repository is installed correctly by calling the discovery API (/alfresco/api/discovery)"
@@ -92,16 +98,19 @@ def test_alfresco_api(host):
         acs_version = acs_version_split[:-1][0]
     assert_that(response['entry']['repository']['version']['display'], contains_string(acs_version))
 
+
 def test_share_log_exists(host):
     "Check that share.log exists in /var/log/alfresco"
     with host.sudo():
         assert_that(host.file("/var/log/alfresco/share.log").exists)
+
 
 def test_share_context_200(host):
     "Check that /share context is available and returns a HTTP 200 status code"
     cmd = host.run("curl -iL --user admin:admin --connect-timeout 5 http://{}:8080/share".format(test_host))
     assert_that(cmd.stdout, contains_string("Alfresco Share"))
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
+
 
 def test_vti_bin_context_200(host):
     "Check that /share context is available and returns a HTTP 200 status code"
@@ -110,6 +119,7 @@ def test_vti_bin_context_200(host):
     assert_that(cmd.stdout, contains_string("This application does not provide a web interface in the browser."))
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
 
+
 def test_vti_inf_context_200(host):
     "Check that /share context is available and returns a HTTP 200 status code"
     cmd = host.run("curl -iL --user admin:admin --connect-timeout 5 http://{}:8080/_vti_inf.html".format(test_host))
@@ -117,16 +127,19 @@ def test_vti_inf_context_200(host):
     assert_that(cmd.stdout, contains_string("FrontPage Configuration Information"))
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
 
+
 def test_api_explorer_context_200(host):
     "Check that /api-explorer context is available and returns a HTTP 200 status code"
     cmd = host.run("curl -iL --user admin:admin http://{}:8080/api-explorer".format(test_host))
     assert_that(cmd.stdout, contains_string("Alfresco Content Services REST API Explorer"))
     assert_that(cmd.stdout, contains_string("HTTP/1.1 200"))
 
+
 def test_keytest_keystore_exists(host):
     "Check that the custom keystore exists in /var/opt/alfresco/content-services/keystore/keystest"
     with host.sudo():
         assert_that(host.file("/var/opt/alfresco/content-services/keystore/keystest").exists)
+
 
 def test_ags_repo_is_installed_and_loaded(host, get_ansible_vars):
     """Check if rm amp is installed in repo war and loaded at startup"""
@@ -143,6 +156,7 @@ def test_ags_repo_is_installed_and_loaded(host, get_ansible_vars):
         assert_that(getlog.contains("Installing module 'alfresco-rm-enterprise-repo' version 3.5.0"))
     assert_that(cmd.stdout, contains_string("AGS Repo\n   -    Version:      3.5.0"))
 
+
 def test_ags_share_is_installed_and_loaded(host, get_ansible_vars):
     """Check if rm amp is installed in share war and loaded at startup"""
     java_version = get_ansible_vars["dependencies_version"]["java"]
@@ -158,6 +172,7 @@ def test_ags_share_is_installed_and_loaded(host, get_ansible_vars):
         assert_that(getlog.contains("AGS Enterprise Share, 3.5.0, Alfresco Governance Services Enterprise Share Extension"))
     assert_that(cmd.stdout, contains_string("AGS Enterprise Share\n   -    Version:      3.5.0" ))
 
+
 def test_environment_jvm_opts(host):
     "Check that overwritten JVM_OPTS are taken into consideration"
     java_processes = host.process.filter(user="alfresco", comm="java")
@@ -166,6 +181,7 @@ def test_environment_jvm_opts(host):
         if 'org.apache.catalina.startup.Bootstrap' in java_process.args:
             assert_that(java_process.args, contains_string('-Xmx900m'))
             assert_that(java_process.args, contains_string('-Xms350m'))
+
 
 def test_mounting_storage(host):
     """Check wether Content Store has been properly mounted as per config."""
@@ -178,6 +194,7 @@ def test_mounting_storage(host):
     if repovars["cs_storage"]["options"]:
         assert_that(set(host.mount_point(dir_root).options) & set(repovars["cs_storage"]["options"].split(',')))
 
+
 def test_newly_added_properties_are_set(host):
     "Check that extra props exists in global properties file"
     with host.sudo():
@@ -186,6 +203,24 @@ def test_newly_added_properties_are_set(host):
     assert_that(b'index.subsystem.name=noindex' in content)
     assert_that(host.socket("tcp://0.0.0.0:1121").is_listening)
 
+
 def test_no_ghostscript(host):
     host.run_expect([127], "gs")
     host.run_expect([127], "ghostscript")
+
+
+def test_share_CSRF_config(host):
+    """Check basic CSRF renderinng in share XML file"""
+    alfresco_cfg_dir = '/etc/opt/alfresco/content-services'
+    share_cfg_dir = 'classpath/alfresco/web-extension'
+    share_cfg_file = '/'.join([alfresco_cfg_dir,
+                              share_cfg_dir,
+                              'share-config-custom.xml'])
+    xmlpath = './config[@condition="CSRFPolicy"]/properties'
+    with host.sudo():
+        share_cfg = host.file(share_cfg_file).content_string
+    share_xml = xmlreader.fromstring(share_cfg)
+    referer = share_xml.findall('{}/referer'.format(xmlpath))
+    origin = share_xml.findall('{}/origin'.format(xmlpath))
+    assert_that(referer[0].text == 'http://localhost:8080/share/.*|https://someotherapp/.*')
+    assert_that(origin[0].text == 'http://localhost:8080,https://someotherapp')
