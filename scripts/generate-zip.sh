91e41f2b9e444a69cdc26936bcf8019257b22faf
Alexandre Chapellon <alexandre.chapellon@hyland.com>
2023-03-30 15:09:16 +0200

    exclude roles' mlecule assets and .envrc

--- scripts/generate-zip.sh
+++ scripts/generate-zip.sh
@@ -10,6 +10,7 @@ REPO_ROOT_DIR="$(realpath "$(dirname "$0")"/..)"
 ARTIFACT_NAME="alfresco-ansible-deployment-${VERSION}"
 
 echo "create distribution folder"
+git clean -f -d
 BUILD_DIR=$(mktemp -d)
 pushd "$BUILD_DIR" && cp -a "${REPO_ROOT_DIR}" "$ARTIFACT_NAME"
 
@@ -20,12 +21,11 @@ echo "Generating ZIP file..."
 find "$ARTIFACT_NAME" \( -type f \
 	! -path "${ARTIFACT_NAME}/.*" \
 	! -path "${ARTIFACT_NAME}/tests/*" \
-	! -path "${ARTIFACT_NAME}/molecule/*" \
+	! -path "${ARTIFACT_NAME}*/molecule/*" \
 	! -name "*.puml" \
 	! -path "${ARTIFACT_NAME}/scripts/*" \
 	! -name ".git*" \
 	! -name alfresco-ansible.pem.enc \
 	-o -path "${ARTIFACT_NAME}/scripts/generate-secret.sh" \
-	-o -path "${ARTIFACT_NAME}/scripts/vagrant_provision.sh" \
-	-o -name .envrc \) \
+	-o -path "${ARTIFACT_NAME}/scripts/vagrant_provision.sh" \) \
 	-exec zip "${REPO_ROOT_DIR}/${ARTIFACT_NAME}.zip" {} +
