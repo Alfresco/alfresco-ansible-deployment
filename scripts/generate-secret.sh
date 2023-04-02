e6eed8adb3a54d8e1277c6b8295fc21300fed146
Alexandre Chapellon <alexandre.chapellon@hyland.com>
2023-04-02 18:05:55 +0200

    remove yaml dodgy chars

--- scripts/generate-secret.sh
+++ scripts/generate-secret.sh
@@ -22,8 +22,10 @@ if [ -z "${SECRET_KEY}" ]; then
 fi
 
 RANDOM_STRING=$(\
+    ANSIBLE_FORCE_COLOR=False \
+    ANSIBLE_NOCOLOR=True \
     ansible -m ansible.builtin.command \
-    -a "echo {{ lookup('password','/dev/null',chars=['ascii_letters','digits','+$?/&\,;()[]:_='],length=33) }}" \
+    -a "echo {{ lookup('password','/dev/null',chars=['ascii_letters','digits','+$?/,;()[]_='],length=33) }}" \
     localhost -o 2>/dev/null \
     | awk '{print $NF}' \
 )
