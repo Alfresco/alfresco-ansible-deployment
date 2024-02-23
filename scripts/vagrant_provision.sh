deb8a7f3bfdb7c519e3583e98aea2c57c491ff61
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2024-02-23 09:55:06 +0100

    fix vagrant [skip ci]

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -3,4 +3,4 @@ curl https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python
 cd /vagrant
 python3 -m pipenv install --deploy
 python3 -m pipenv run ansible-galaxy install -r requirements.yml
-python3 -m pipenv run ansible-playbook -i inventory_local.yml -e "autogen_unsecure_secrets=true" playbooks/acs.yml
+python3 -m pipenv run ansible-playbook -i inventory_local.yml -e "autogen_unsecure_secrets=true" -e "known_urls=[http://localhost]" playbooks/acs.yml
