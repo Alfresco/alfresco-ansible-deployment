bc313a9a94b5be566c7c3a920df9c88917911813
pmacius <pawel.maciusiak@hyland.com>
2025-03-11 09:48:16 +0100

    OPSEXP-3162 Replace extra-vars files with the acs_play_major_version var

--- roles/repository/molecule/default/tests/test_repository.py
+++ roles/repository/molecule/default/tests/test_repository.py
@@ -20,8 +20,6 @@ def get_ansible_vars(host):
     java_role = "file=../java/vars/main.yml name=java_role"
     common_vars = "file=../../../common/vars/main.yml name=common_vars"
     common_defaults = "file=../../../common/defaults/main.yml name=common_defaults"
-    group_vars = "file=../../../../group_vars/all.yml name=group_vars"
-    ansible_vars = host.ansible("include_vars", group_vars)["ansible_facts"]["group_vars"]
     ansible_vars.update(host.ansible("include_vars", common_defaults)["ansible_facts"]["common_defaults"])
     ansible_vars.update(host.ansible("include_vars", common_vars)["ansible_facts"]["common_vars"])
     ansible_vars.update(host.ansible("include_vars", java_role)["ansible_facts"]["java_role"])
