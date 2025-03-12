6dd76c505cdd132c3ba30541ddf4911650cf1145
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2025-03-12 17:56:53 +0100

    Bump vagrant integration
    * Use bento box which also supports arm64
    * Use a different box depending on the acs_major_version
    * Allow chosing ACS version by setting VAGRANT_ACS_MAJOR_VERSION

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,6 +1,12 @@
-#!/bin/bash -e
-curl https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python3
+#!/bin/bash -ex
+sudo apt-get update -q
+sudo apt-get install python3-pip -qy
 cd /vagrant
+pip install --user pipenv
 python3 -m pipenv install --deploy
 python3 -m pipenv run ansible-galaxy install -r requirements.yml
-python3 -m pipenv run ansible-playbook -i inventory_local.yml -e "autogen_unsecure_secrets=true" -e "acs_play_known_urls=[http://localhost]" playbooks/acs.yml
+python3 -m pipenv run ansible-playbook -i inventory_local.yml \
+    -e "acs_play_major_version=${VAGRANT_ACS_MAJOR_VERSION}" \
+    -e "autogen_unsecure_secrets=true" \
+    -e "acs_play_known_urls=[http://localhost]" \
+    playbooks/acs.yml
